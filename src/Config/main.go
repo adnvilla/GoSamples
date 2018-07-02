05127e5ae67debe9532f1c37f8c0eb7b6a70b1ca
AdnVilla <adnvilla@gmail.com>
2018-07-01 22:08:56 -0500

    Added sample configuration

---
+++ src/Config/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+// Configuration schema
+type Configuration struct {
+	Users  []string
+	Groups []string
+	Name   string
+}
+
+func main() {
+
+	file, _ := os.Open("config.json")
+	defer file.Close()
+	decoder := json.NewDecoder(file)
+	configuration := Configuration{}
+	err := decoder.Decode(&configuration)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	fmt.Println(configuration.Users)
+	fmt.Println(configuration.Name)
+}
